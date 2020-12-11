ad77125e97e51aea28a167032b6080b279dd7b13
steelbridge-io <chris@steelbridge.io>
2020-12-11 14:38:19 -0800

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -44,7 +44,7 @@ var paths = {
         dest: 'languages/the-trinity-guide-theme.pot'
     },
     site: {
-        url: 'http://bootstrap.test'
+        url: 'http://trinityguide.local'
     }
 }
 
